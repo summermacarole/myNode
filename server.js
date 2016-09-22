84a329102593504d4283db89e879d4b089bd2d42
littlemacarole <1740249919@qq.com>
2016-09-22 22:17:56 +0800

    系统初始化，删除以前的文件

--- server.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by littlemacarole on 2016/8/17.
- */
-var http=require('http');
-http.createServer(function(req,res){
-    res.write('hello');
-    res.end('respond end');
-}).listen(3000,'localhost');
-console.log('your server is running');
