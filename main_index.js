135bf414e5ce4fb449c51c60db40a8b0b3653a89
littlemacarole <1740249919@qq.com>
2016-10-15 23:49:00 +0800

    node2分支初始化

--- main_index.js
+++
@@ -1,7 +0,0 @@
-var fs=require('fs'),
-    url=require('url')
-exports.mainIndex=function(res,req){
-    var readPath=url.parse(req.url).pathname;
-    var indexPage=fs.readFileSync('index.html')
-    res.end(indexPage)
-}
